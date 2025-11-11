37e5b4802a83821e7bfd10ed00845f747a4950d2
Piyush Khanduri <118597419+Piyush66Khanduri@users.noreply.github.com>
2025-11-11 23:21:22 +0530

    Update Chat_and_file_transfer_API.py

--- Chat_and_file_transfer_API.py
+++ Chat_and_file_transfer_API.py
@@ -453,5 +453,7 @@ def get_files():
 def index():
     return "Piyush's API "
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
